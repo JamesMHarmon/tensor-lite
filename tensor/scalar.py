6ff2fe79293352a02b9c447e88521ccc545c0a03
James Harmon <jamesmharmon@gmail.com>
2023-02-08 10:12:32 -0800

    Add sigmoid function

--- tensor/scalar.py
+++ tensor/scalar.py
@@ -6,10 +6,37 @@ from tensor.grad import grad
 Scalarable = Union['Scalar', float]
 
 class Scalar:
-    """ Represents a single scalar value """
+    """
+    Represents a single scalar value in the tensor graph.
+
+    Attributes:
+        data (float): The value of the scalar.
+        grad (float): The gradient of the scalar with respect to its inputs.
+    """
     def __init__(self, data: float) -> None:
+        """
+        Initialize a new scalar value.
+
+        Args:
+            data (float): The value of the scalar.
+        """
         self.data = data
         self.grad = 0.0
+
+    def parents(self):
+        """ Returns the parent nodes of this node in the tensor graph. """
+        return ()
+
+    def backward(self):
+        """ Computes the gradient of this scalar with respect to its inputs. The gradient is accumulated in the `grad` attribute. """
+        return grad(self)
+
+    def _backward(self, grad):
+        """ Computes the gradients to be passed backward to the parent nodes. """
+        return ()
+
+    def sigmoid(self) -> 'Scalar':
+        return Sigmoid(self)
 
     def __add__(self, other: Scalarable) -> 'Scalar':
         return Add(self, other)
@@ -19,16 +46,6 @@ class Scalar:
 
     def __pow__(self,  exponent: float) -> 'Scalar':
         return Pow(self, exponent)
-
-    def parents(self):
-        return ()
-
-    def backward(self):
-        """ Compute the gradient of this scalar with respect to its inputs. The gradient is accumulated in the `grad` attribute. """
-        return grad(self)
-
-    def _backward(self, grad):
-        return ()
 
     def __radd__(self, other: Scalarable) -> 'Scalar':
         return self + other
@@ -81,7 +98,6 @@ class Multiply(Scalar):
         super().__init__(product)
 
     def _backward(self, grad: float):
-        # Gradient is propagated from the right nodes data to the left node and vice versa.
         return (self._right.data * grad, self._left.data * grad)
 
     def parents(self):
@@ -104,3 +120,16 @@ class Pow(Scalar):
 
     def parents(self):
         return (self._base, self._exp)
+
+class Sigmoid(Scalar):
+    def __init__(self, value):
+        self._value = self._as_scalar(value)
+        sigmoid = 1 / (1 + math.exp(-self._value.data))
+
+        super().__init__(sigmoid)
+
+    def _backward(self, grad: float):
+        return (grad * (self.data * (1 - self.data)), )
+
+    def parents(self):
+        return (self._value, )
