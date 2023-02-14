afc834146693a3dc856f9d9f7957e92f63f43c7f
James Harmon <jamesmharmon@gmail.com>
2023-02-13 22:34:55 -0800

    Add accuracy metric

--- examples/neural-net.py
+++ examples/neural-net.py
@@ -1,5 +1,4 @@
 def main():
-    from collections import namedtuple
     from tensor import Scalar, SGD
     import random as ran
     import math
@@ -54,6 +53,18 @@ def main():
 
         return loss / len(predicted)
 
+    def calculate_accuracy(size):
+        X = [[ran.uniform(-2.0, 2.0) for _ in range(num_inputs)] for _ in range(size)]
+        Y = [target_fn(*inputs) for inputs in X]
+
+        # Convert the predictions to binary values (0 or 1) using a threshold of 0.5
+        binary_predictions = [1.0 if forward_pass(inputs, layers)[0].data >= 0.5 else 0.0 for inputs in X]
+
+        # Calculate the accuracy
+        num_correct = sum(1 for binary_prediction, target in zip(binary_predictions, Y) if target == binary_prediction)
+        accuracy = num_correct / len(Y)
+        return accuracy
+
     """ Define the size of the neural net. Here we have an input of 2 neurons, followed by a hidden layer of 8 neurons, and then an output of 1 neuron(s). """
     num_inputs, hidden_layer_size, num_outputs = 2, 4, 1
     layers = [
@@ -76,20 +87,22 @@ def main():
 
     # Perform n number of steps to optimize the network.
     for step in range(1, num_steps + 1):
+        optimizer.zero_grad()
 
         # For each step, perform a forward pass with the input sample data.
         X = [[ran.uniform(-2.0, 2.0) for _ in range(num_inputs)] for _ in range(batch_size)]
         Y = [[target_fn(*inputs)] for inputs in X]
         predicted = [forward_pass(inputs, layers) for inputs in X]
+
         # Calculate the loss between the target values and the predicted values. Use either mse_loss or binary_cross_entropy_loss here.
         losses = [binary_cross_entropy_loss(target, predicted) for target, predicted in zip(Y, predicted)]
         total_loss = sum(losses) / batch_size
         total_loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
 
         if step in [1, num_steps] or step % 100 == 0:
-            print(f'Step={step}, Loss={total_loss.data: .04f}')
+            accuracy = calculate_accuracy(100)
+            print(f'Step={step}, Loss={total_loss.data: .04f}, Accuracy={accuracy:.1%}')
 
 if __name__ == '__main__':
     import sys, os
